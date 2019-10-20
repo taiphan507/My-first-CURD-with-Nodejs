6298f9c3f448941a0fe215ddc8a922bddac7ac90
coder <huutai050798@gmail.com>
2019-10-20 20:20:51 +0700

    fix form login account

--- index.js
+++ index.js
@@ -1,15 +1,29 @@
+require('dotenv').config();
 var express = require('express');
 var app = express();
+var mongoose = require('mongoose');
+
+var userRoute = require('./routes/user.route');
+console.log(userRoute);
+
 
 var port = 3000;
+
+
 
 app.set('view engine', 'pug');
 app.set('views', './views');
 
 
+
+mongoose.connect(process.env.MONGO_URL);
+
+
 app.get('/', function (req, res) {
     res.render('index');
 })
+
+app.use('/users', userRoute);
 
 app.listen(port, function () {
     console.log('Server listening on port ' + port);
